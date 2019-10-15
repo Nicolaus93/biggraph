3c639891eba8f04ee7817fc9a0c2e58068130984
nicoloc <nico.campolongo@gmail.com>
2019-10-15 14:28:07 +0200

    kmeans using faiss

---
+++ faiss_kmeans.py
@@ -0,0 +1,51 @@
+import numpy as np
+import time
+import faiss
+
+
+def train_kmeans(x, k, ngpu=4):
+    "Runs kmeans on one or several GPUs"
+    d = x.shape[1]
+    clus = faiss.Clustering(d, k)
+    clus.verbose = True
+    clus.niter = 20
+
+    # otherwise the kmeans implementation sub-samples the training set
+    clus.max_points_per_centroid = 10000000
+
+    res = [faiss.StandardGpuResources() for i in range(ngpu)]
+
+    flat_config = []
+    for i in range(ngpu):
+        cfg = faiss.GpuIndexFlatConfig()
+        cfg.useFloat16 = False
+        cfg.device = i
+        flat_config.append(cfg)
+
+    if ngpu == 1:
+        index = faiss.GpuIndexFlatL2(res[0], d, flat_config[0])
+    else:
+        indexes = [faiss.GpuIndexFlatL2(res[i], d, flat_config[i])
+                   for i in range(ngpu)]
+        index = faiss.IndexReplicas()
+        for sub_index in indexes:
+            index.addIndex(sub_index)
+
+    # perform the training
+    clus.train(x, index)
+    centroids = faiss.vector_float_to_array(clus.centroids)
+
+    obj = faiss.vector_float_to_array(clus.obj)
+    print("final objective: %.4g" % obj[-1])
+
+    return centroids.reshape(k, d)
+
+
+if __name__ == "__main__":
+    t0 = time.time()
+    x = np.random.rand(100, 100)
+    k = 5
+    train_kmeans(x, k)
+    t1 = time.time()
+
+    print("total runtime: %.3f s" % (t1 - t0))
