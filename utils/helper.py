6cc6b2fe73573915277203b15987a210f18e2736
nicoloc <nico.campolongo@gmail.com>
2019-10-15 17:15:42 +0200

    moved load_data

--- utils/helper.py
+++ utils/helper.py
@@ -1,4 +1,6 @@
 import json
+import h5py
+import numpy as np
 
 
 def iter_partitions(model_path):
@@ -18,3 +20,16 @@ def iter_partitions(model_path):
         with open(count_file, "rt") as f:
             count = int(f.readline())
         yield model_path / 'embeddings_link_{}.v50.h5'.format(i), count
+
+
+def load_data(model_path):
+
+    x_arrays = []
+    y_arrays = []
+    for partition, count in iter_partitions(model_path):
+        h5f = h5py.File(partition, 'r')
+        X = h5f["embeddings"][:]
+        Y = h5f["labels"][:]
+        x_arrays.append(X)
+        y_arrays.append(Y)
+    return np.vstack(x_arrays), np.hstack(y_arrays)
