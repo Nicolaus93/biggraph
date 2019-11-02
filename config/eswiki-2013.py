95da95831933fcf230923434f3574bfa9f14f543
nicoloc <nico.campolongo@gmail.com>
2019-11-02 01:37:17 +0100

    eswiki config

---
+++ config/eswiki-2013.py
@@ -0,0 +1,49 @@
+from os.path import join
+
+BASENAME = "eswiki-2013"
+
+
+def get_torchbiggraph_config():
+
+    model = join("/data/models", BASENAME)
+    graphs = join("data/graphs", BASENAME)
+
+    config = dict(
+        # I/O data
+        entity_path=model,
+        # where to store learnt edges
+        edge_paths=[
+            join(graphs, "train_partitioned"),
+            join(graphs, "test_partitioned"),
+        ],
+        checkpoint_path=model,
+
+        # Graph structure
+        entities={
+            'link': {'num_partitions': 10},
+        },
+        relations=[{
+            'name': 'follow',
+            'lhs': 'link',
+            'rhs': 'link',
+            'operator': 'none',
+        }],
+
+        verbose=True,
+        dynamic_relations=False,
+
+        # Scoring model
+        dimension=128,
+        global_emb=False,
+
+        # Training
+        num_epochs=100,
+        num_uniform_negs=100,
+        loss_fn='softmax',
+        lr=0.01,
+
+        # Misc
+        hogwild_delay=2,
+    )
+
+    return config
